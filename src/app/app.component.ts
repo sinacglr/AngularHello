4e30bbc820cb747369e39e9db4372175cf1f2e14
midnight-child <sinacglr@gmail.com>
2023-08-15 20:43:30 +0300

    Demo

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: '<h1>Hello World</h1>',
+})
+export class AppComponent {
+  title = 'AngularProject';
+}
